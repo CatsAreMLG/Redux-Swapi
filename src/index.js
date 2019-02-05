5acf34b123716dd8f0be4e946e7154380ea82c0d
CatsAreMLG <joshuadgon@gmail.com>
2019-02-04 22:35:56 -0600

    Move imports around

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./reducers";
-
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+
+import "./index.css";
+import App from "./App";
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
